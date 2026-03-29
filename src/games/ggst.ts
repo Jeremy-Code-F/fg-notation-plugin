5280975ea1fbf87bbfb54ee5982e43222c46fa22
chewy <jeremy.farmer1206@proton.me>
2026-03-29 18:24:38 -0400

    feat: More GGST icons, K and Dust support

--- src/games/ggst.ts
+++ src/games/ggst.ts
@@ -1,5 +1,7 @@
 import { GameConfig } from "../game-config";
 import GGST_P_PNG from "../assets/GGST-P.png";
+import GGST_K_PNG from "../assets/GGST-K.png";
+import GGST_D_PNG from "../assets/GGST-D.png";
 
 export const GGST_CONFIG: GameConfig = {
 	buttonPattern: "HS|S|P|K|D",
@@ -7,10 +9,10 @@ export const GGST_CONFIG: GameConfig = {
 	modifierBadgeRe: /^(\[CH\]|\[RISC\])$/,
 	buttonData: {
 		P: { label: "P", cssClass: "ggst-p", png: GGST_P_PNG },
-		K: { label: "K", cssClass: "ggst-k" },
+		K: { label: "K", cssClass: "ggst-k", png: GGST_K_PNG },
 		S: { label: "S", cssClass: "ggst-s" },
 		HS: { label: "HS", cssClass: "ggst-hs" },
-		D: { label: "D", cssClass: "ggst-d" },
+		D: { label: "D", cssClass: "ggst-d", png: GGST_D_PNG },
 		RC: { label: "RC", cssClass: "ggst-rc" },
 		Burst: { label: "Burst", cssClass: "ggst-burst" },
 		THROW: { label: "THROW", cssClass: "ggst-throw" },
